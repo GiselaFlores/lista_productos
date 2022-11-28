d5b0744583ecd2ea855643e0c038b9011a2ad858
GiselaFlores <gi.flores@bue.edu.ar>
2022-11-28 10:39:51 -0300

    tabla

--- src/Componentes/Mostrar.js
+++ src/Componentes/Mostrar.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-import {collection, getDocs, getDoc, deleteDoc } from 'firebase/firestore';
+import {collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore';
 import {db} from '../firebaseConfig/firebase';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import { async } from '@firebase/util';
 const MySwal = withReactContent(Swal);
 
 
@@ -19,7 +20,7 @@ const Mostrar = () => {
     //3 creamos la función para mostrar los documentos con asincronismo
 
     const getProductos = async () => {
-        const data = await getDocs(productosCollection);
+        const data = await getDocs(productosCollection)
         console.log(data.docs);
 
         setProductos(
@@ -28,17 +29,56 @@ const Mostrar = () => {
         console.log(productos);
     }
 
+    // 4 creamos una función para eliminar los documentos
+
+    const deleteProducto = async (id) => {
+        const productosDoc = doc(db, "products", id);
+        await deleteDoc(productosDoc);
+        getProductos();
+    }
+
+    // 5 configuración para los alerts
+
+
     //6 aplicamos el useEffect para la bajada 
 
     useEffect( ()=>{
         getProductos()
     }, []);
 
+    // 7 mostrar los datos en formato tabla
   return (
-    <div>
-      
+    <div className='container'>
+        <div className='row'>
+            <div className='col'>
+                <div className='d-grid gap-2'>
+                    <link to="/crearproducto" className='btn btn-secondary mt-2 mb-2' > Crear producto</link>
+                </div>
+                <table className='table table-dark table-hover'>
+                    <thead>
+                        <tr>
+                            <th>Descripción</th>
+                            <th>Stock</th>
+                            <th>Actiones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { productos.map((produc)=>(
+                            <tr key={produc.id}>
+                                <td key={produc.descripcion}></td>
+                                <td key={produc.stock}></td>
+                                <td>
+                                    <link to={`/editarproducto/${produc.id}`} className="btn btn-light"><i class="fa-solid fa-trash"></i></link>
+                                    <button><i class="fa-solid fa-trash"></i></button>
+                                </td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-  )
-}
+  )}
 
 export default Mostrar;

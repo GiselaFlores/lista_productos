80c41b786b88757a4803c23ac9001390df188007
GiselaFlores <gi.flores@bue.edu.ar>
2022-11-28 19:32:36 -0300

    modificación

--- src/Componentes/Mostrar.js
+++ src/Componentes/Mostrar.js
@@ -1,63 +1,67 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from "react";
 import {Link} from 'react-router-dom';
-import {collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, getDoc, deleteDoc, doc} from 'firebase/firestore';
 import {db} from '../firebaseConfig/firebase';
 import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { async } from '@firebase/util';
+import {async} from '@firebase/util';
+import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 
 
 const Mostrar = () => {
-    //1 configuración de los hooks que van a mostrar
+
+    // 1 configuración de los hook de mostrar 
 
     const [productos, setProductos] = useState([]);
 
-    //2 referenciamos a la bd de firebase
+    // 2 referenciamos a la db de firebase
 
-    const productosCollection = collection(db, "products");
+    const productosColletion = collection(db, "Productos");
 
-    //3 creamos la función para mostrar los documentos con asincronismo
+    // 3 creamos la función para mostrar los documentos con asincronismo
 
     const getProductos = async () => {
-        const data = await getDocs(productosCollection)
-        console.log(data.docs);
+        const data = await getDocs(productosColletion)
+        console.log(data.docs)
 
         setProductos(
-        data.docs.map( (doc) => ({...doc.data(), id:doc.id}))
+            data.docs.map( (doc)=>({id:doc.id}))
         );
         console.log(productos);
     }
 
-    // 4 creamos una función para eliminar los documentos
+    // 4 crear la función para eliminar registros
 
-    const deleteProducto = async (id) => {
-        const productosDoc = doc(db, "products", id);
-        await deleteDoc(productosDoc);
+    const deleteProducto = async (id) =>{
+        const productoDoc = doc(db, "Productos", id);
+        await deleteDoc(productoDoc);
         getProductos();
     }
 
-    // 5 configuración para los alerts
+    //5 configuración sweetalert
 
 
-    //6 aplicamos el useEffect para la bajada 
+    //6 declaramos el useeffect
 
-    useEffect( ()=>{
-        getProductos()
-    }, []);
+    useEffect( ()=> {
+        getProductos();
+    }, [])
 
-    // 7 mostrar los datos en formato tabla
+
+    //7 mostrar datos estructura
+
   return (
     <div className='container'>
         <div className='row'>
             <div className='col'>
                 <div className='d-grid gap-2'>
-                    <link to="/crearproducto" className='btn btn-secondary mt-2 mb-2' > Crear producto</link>
+                    <link to="/crearproducto" className='btn btn-secondary mt-2 mb-2' ><i className="fa-solid fa-plus"></i></link>
                 </div>
                 <table className='table table-dark table-hover'>
                     <thead>
                         <tr>
-                            <th>Descripción</th>
+                            <th>Nombre</th>
+                            <th>Precio</th>
                             <th>Stock</th>
                             <th>Actiones</th>
                         </tr>
@@ -65,20 +69,21 @@ const Mostrar = () => {
                     <tbody>
                         { productos.map((produc)=>(
                             <tr key={produc.id}>
-                                <td key={produc.descripcion}></td>
-                                <td key={produc.stock}></td>
+                                <td key={produc.Nombre}></td>
+                                <td key={produc.Precio}></td>
+                                <td key={produc.Stock}></td>
                                 <td>
-                                    <link to={`/editarproducto/${produc.id}`} className="btn btn-light"><i class="fa-solid fa-trash"></i></link>
-                                    <button><i class="fa-solid fa-trash"></i></button>
+                                    <Link to={`/editarproducto/${produc.id}`} className="btn btn-light"><i className="fa-solid fa-pen-to-square"></i></link>
+                                    <button><i className="fa-solid fa-trash"></i></button>
                                 </td>
                             </tr>
                         ))}
-
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-  )}
+  )
+}
 
 export default Mostrar;
